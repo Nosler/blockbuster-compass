a4c1df8a977f942c7cac2f93f34f43a074d730b1
alex-mcknight11 <alex.mcknight11@gmail.com>
2023-02-19 13:32:26 -0500

    initialize project

---
+++ blockbustercompass/blockbustercompass/blockbustercompass/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using blockbustercompass.Services;
+using blockbustercompass.Views;
+
+namespace blockbustercompass
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
---
+++ blockbustercompass/blockbustercompass/blockbustercompass/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using blockbustercompass.Models;
+using blockbustercompass.ViewModels;
+
+namespace blockbustercompass.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
