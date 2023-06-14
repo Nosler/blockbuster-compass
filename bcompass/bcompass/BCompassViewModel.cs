ae05c691c6b5627433be5b9898aa69a059f3b843
Andrea Nosler <andinosler@gmail.com>
2023-06-13 20:44:29 -0400

    messages

--- bcompass/bcompass/BCompassViewModel.cs
+++ bcompass/bcompass/BCompassViewModel.cs
@@ -218,7 +218,8 @@ namespace bcompass
             Console.WriteLine(messages);
             foreach((string, double, double) row in messages)
             {
-                if (true)
+                if ((row.Item2 == 0 && row.Item3 == 0) ||
+                    (row.Item2 <= distanceFromBB && row.Item3 >= distanceFromBB))
                 {
                     returnList.Add(row.Item1);
                 }
@@ -229,57 +230,7 @@ namespace bcompass
         public void SetMessage(double dist)
         {
             var data = new (string, double, double)[] {
-                ("Blockbuster!!!", 0, 0),
-                ("Blockbuster is waiting for you!", 0, 0),
-                ("A trip to Blockbuster is fun for the whole family!", 0, 0),
-                ("Have you heard of this cool place called Blockbuster?", 0, 0),
-                ("Fun Fact: You can get Movies at your local Blockbuster!", 0, 0),
-                ("Overdue tapes at your place? Better make it to your nearest Blockbuster!", 0, 0),
-                ("Make it a Blockbuster Hike!", 0, 0),
-                ("Go to Blockbuster!", 0, 0),
-                ("Go to Blockbuster.", 0, 0),
-                ("You should go to Blockbuster!", 0, 0),
-                ("You should go to Blockbuster.", 0, 0),
-                ("Kellyanne loved going to Blockbuster. I miss her so much.", 0, 0),
-                ("Don't forget to return some videotapes!", 0, 0),
-                ("Don't forget to return your tapes!", 0, 0),
-                ("Looking for the hottest new tapes and DVDs? We have them at Blockbuster!", 0, 0),
-                ("Not sure what to get from Blockbuster? We have a Call-gorithm!", 0, 0),
-                ("Blockbuster gets new releases 30 days before Netflix!", 0, 0),
-                ("Have you thought about Blockbuster today?", 0, 0),
-                ("Hey! Blockbuster!", 0, 0),
-                ("Did you know: Blockbuster!!!", 0, 0),
-                ("Blockbuster? Blockbuster!", 0, 0),
-                ("Are you ready for some Blockbuster?", 0, 0),
-                ("Come to Blockbuster and you'll discover what a difference it makes!", 0, 0),
-                ("Wow, what a difference! Blockbuster Video!", 0, 0),
-                ("I have to return some video tapes.", 0, 0),
-                ("You can find the widest selection of movies around at Blockbuster!", 0, 0),
-                ("Who loves blockbuster? You do!", 0, 0),
-                ("We don't track your location, but if you go to Hollywood Video, we can't make any promises.", 0, 0),
-                ("B-L-O-C-K-B-U-S-T-E-R! What's that spell? Read it!", 0, 0),
-                ("The future is now with Blockbuster Video!", 0, 0),
-                ("Don't Bust Your Block, Block Those Busts! (note - figure out better phrase)", 0, 0),
-                ("Be sure to go to Blockbuster, because you can't watch movies on your phone!", 0, 0),
-                ("Blockbuster is here to stay!", 0, 0),
-                ("With so many locations, it can be hard to find your closest store! Luckily, Blockbuster Compass is here for you!", 0, 0),
-                ("Blockbuster is here to stay!", 0, 0),
-                ("Guess what we're thinking of! (Here's a hint: It's Blockbuster!)", 0, 0),
-                ("Have you been to your nearest Blockbuster lately?", 0, 0),
-                ("What are movies? find out at Blockbuster!", 0, 0),
-                ("Movies? Who needs 'em? You do! From Blockbuster!", 0, 0),
-                ("Remember that commercial where the tapes all come to life and hang out at night? That doesn't really happen.", 0, 0),
-                ("BustBlockter", 0, 0),
-                ("Who needs slow streaming? With Blockbuster, you can get real DVDs, real fast!", 0, 0),
-                ("Get some tapes and keep 'em for a week!", 0, 0),
-                ("When the world ends and the internet streams no more, we'll still be here.", 0, 0),
-                ("Looking for a video? Blockbuster has those!", 0, 0),
-                ("We have over ten thousand videos!", 0, 0),
-                ("Looking for a video? Blockbuster has it!", 0, 0),
-                ("Video stores aren't going anywhere, so be sure you know where yours are!", 0, 0),
-                ("Looking for a video? Blockbuster has it! We have over ten thousand videos!", 0, 0),
-                ("Feeling a little far from Blockbuster? Go a little crazy and consider swinging by!", 3000, 9999),
-                ("Head on over to Blockbuster video, and you’ll see just what a difference!", 3000, 9999)
+                ("Blockbuster!!!", 0, 0)
             };
             List<string> messages = this.BuildMessagesFromDistance(data, dist);
             Random rnd = new Random();
